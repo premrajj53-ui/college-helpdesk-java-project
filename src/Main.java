81f7afb1706d00e84733b77e8c9b668152ceeb6f
prem <premrajj53@gmail.com>
2026-03-28 23:29:43 +0530

    Add initial complaint class with attributes and display method

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String []args){
+        Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
+       while (isRunning) {
+           System.out.println(":---Help-Desk---:");
+           System.out.println("Login as :");
+           System.out.println("1.Student");
+           System.out.println("2.Admin");
+           System.out.println("3.Exit");
+           System.out.println("Enter your choise: 1 , 2 ,3 :");
+           int choise = sc.nextInt();
+           switch (choise) {
+               case 1:
+                   System.out.printf("\n---Student portal---");
 
-
-
+                   break;
+               case 2:
+                   System.out.println("---Admin portal---");
+                   break;
+               case 3:
+                   System.out.printf("Exiting....");
+                   isRunning = false;
+                   break;
+               default:
+                   System.out.println("invalid choise. Enter : 1 , 2 or 3");
+           }
+       }
     }
 
 }
