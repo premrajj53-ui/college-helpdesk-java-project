1f3cc3e81230fbccb87eea94d811a7b6b27cb201
prem <premrajj53@gmail.com>
2026-03-29 21:21:54 +0530

    implemented student complaint registration with HashMap as storage

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,42 @@
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String []args){
         Scanner sc = new Scanner(System.in);
         boolean isRunning = true;
+        HashMap <Integer,Complaint> comlaintmap = new HashMap<>();
+        int compId = 1;
        while (isRunning) {
            System.out.println(":---Help-Desk---:");
            System.out.println("Login as :");
            System.out.println("1.Student");
            System.out.println("2.Admin");
            System.out.println("3.Exit");
-           System.out.println("Enter your choise: 1 , 2 ,3 :");
-           int choise = sc.nextInt();
-           switch (choise) {
+           System.out.println("Enter your choice: 1 , 2 ,3 :");
+           int choice = sc.nextInt();
+           switch (choice) {
                case 1:
-                   System.out.printf("\n---Student portal---");
+                   System.out.println("\n---Student portal---");
+                   System.out.print("Enter Student Id :");
+                   int stId = sc.nextInt();
+                   sc.nextLine();
+                   System.out.println("Describe your issue : ");
+                   String desc = sc.nextLine();
+                   Complaint c1 = new Complaint(compId,desc,"pending",stId);
+
+               comlaintmap.put(compId,c1);
+                   System.out.println("\n------------------------------------");
+                   System.out.println("Complaint raised successfully!");
+                   System.out.println("------------------------------------");
+
+                   System.out.println("Complaint Id :" + compId);
+                   System.out.println("Complaint :" + desc);
+                   System.out.println("Student Id :" + stId);
+                   System.out.println("------------------------------------");
+                   System.out.println("Press Enter to return to the main menu...");
+                   sc.nextLine();
+
 
                    break;
                case 2:
